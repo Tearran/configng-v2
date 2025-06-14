fde047ecba7f765f3de0c784da56312ad0892108
tearran <tearran@gmail.com>
2025-06-14 00:36:16 +0000

    modified:   tools/staging_setup_scaffold.sh

--- tools/staging_setup_scaffold.sh
+++ tools/staging_setup_scaffold.sh
@@ -56,17 +56,18 @@ cat > "${STAGING_DIR}/test_${MODULE}.sh" <<EOF
 #!/bin/bash
 # ${MODULE} - Armbian Config V3 test
 
-${MODULE}() {
-  # TODO: implement module logic
-  echo "Module '${MODULE}' called"
-}
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "${MODULE} - Armbian Config V3 test"
+  echo "Scaffold test"
+  exit 1
+fi
+
 EOF
 
 # Output .sh module template inside ./staging
 cat > "${STAGING_DIR}/doc_${MODULE}.md" <<EOF
-# ${MODULE} - Armbian Config V3 module
+# ${MODULE} - Armbian Config V3 extra documents
 
 EOF
-
 
 echo -e "Staging: Complete\nScaffold for ${MODULE} can be found at ${STAGING_DIR}/."
