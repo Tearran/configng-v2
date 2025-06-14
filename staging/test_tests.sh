af3c25e94471c559c422d4d7ca071944e558cf82
Joey Turner <Tearran@users.noreply.github.com>
2025-06-14 07:49:33 +0000

    new file:   staging/doc_tests.md new file:   staging/metatests.conf new file:   staging/src_tests.sh new file:   staging/test_tests.sh

---
+++ staging/test_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# tests - Armbian Config V3 test
+
+if [[ "tools/staging_setup_scaffold.sh" == "tools/staging_setup_scaffold.sh" ]]; then
+	echo "tests - Armbian Config V3 test"
+	echo "Scaffold test"
+	exit 1
+fi
+
