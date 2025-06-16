6ec86c17956b58bd3e64a4788725af4be6afdaad
tearran <tearran@gmail.com>
2025-06-16 00:52:42 +0000

    modified:   tests/test_checkpoint.sh

--- tests/test_checkpoint.sh
+++ tests/test_checkpoint.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	DEBUG=${DEBUG:-}
 	UXMODE=${UXMODE:-}
-	. src/initialize/checkpoint.sh
+	SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+	source "$SCRIPT_DIR/../src/initialize/checkpoint.sh"
 	checkpoint reset
 	checkpoint mark "Initializing script"
 	sleep 1
