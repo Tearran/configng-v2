99903dbf4c89bc988b1021729fdf11b81f6ea3c5
tearran <tearran@gmail.com>
2025-06-16 01:48:31 +0000

    modified:   tests/test_debug.sh

--- tests/test_debug.sh
+++ tests/test_debug.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 # debug - Armbian Config V3 test
 
-#!/bin/bash
-set -euo pipefail
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	DEBUG=${DEBUG:-true}
 	SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
