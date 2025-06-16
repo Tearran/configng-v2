8d1a583af0d1bdd86c8d38ff224662f7ca372c15
tearran <tearran@gmail.com>
2025-06-16 01:43:57 +0000

    modified:   tests/test_debug.sh

--- tests/test_debug.sh
+++ tests/test_debug.sh
@@ -8,7 +8,8 @@ set -euo pipefail
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	DEBUG=${DEBUG:-true}
 	SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-	source "$SCRIPT_DIR/debug.sh"
+	#source "$SCRIPT_DIR/debug.sh"
+	source "$SCRIPT_DIR/../src/initialize/debug.sh"
 	debug reset
 	debug "Wait one second"
 	sleep 1
