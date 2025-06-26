e6d1577ba2d514962fae14c46c82c1f520a357d4
Joey Turner <Tearran@users.noreply.github.com>
2025-06-26 03:42:55 +0000

    modified:   staging/adjust_motd.conf

--- staging/adjust_motd.sh
+++ staging/adjust_motd.sh
@@ -1,48 +1,51 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# adjust_motd - Armbian Config V2 module
-
-motd_desc() {
-	case "$1" in
-		clear)
-			echo "Clear screen on login"
-			;;
-		header)
-			echo "Show header with logo"
-			;;
-		sysinfo)
-			echo "Display system information"
-			;;
-		tips)
-			echo "Show Armbian team tips"
-			;;
-		commands)
-			echo "Show recommended commands"
-			;;
-		*)
-			echo "No description"
-			;;
-	esac
-}
-
-# Read status for a given MOTD item
-motd_status() {
-	source /etc/default/armbian-motd
-	if [[ "${MOTD_DISABLE:-}" == *"$1"* ]]; then
-		echo "OFF"
-	else
-		echo "ON"
-	fi
-}
-
+#
+# @description Toggle message of the day items
+#
 adjust_motd() {
+
+	# show motd description
+	motd_desc() {
+		case $1 in
+			clear)
+				echo "Clear screen on login"
+				;;
+			header)
+				echo "Show header with logo"
+				;;
+			sysinfo)
+				echo "Display system information"
+				;;
+			tips)
+				echo "Show Armbian team tips"
+				;;
+			commands)
+				echo "Show recommended commands"
+				;;
+			*)
+				echo "No description"
+				;;
+		esac
+	}
+
+	# read status
+ 	motd_status() {
+		source /etc/default/armbian-motd
+		if [[ $MOTD_DISABLE == *$1* ]]; then
+			echo "OFF"
+		else
+			echo "ON"
+		fi
+	}
+
 	LIST=()
-	for v in $(grep THIS_SCRIPT= /etc/update-motd.d/* | cut -d"=" -f2 | sed 's/"//g'); do
-		LIST+=("$v" "$(motd_desc "$v")" "$(motd_status "$v")")
+	for v in $(grep THIS_SCRIPT= /etc/update-motd.d/* | cut -d"=" -f2 | sed "s/\"//g"); do
+		LIST+=("$v" "$(motd_desc $v)" "$(motd_status $v)")
 	done
 
-	INLIST=($(grep THIS_SCRIPT= /etc/update-motd.d/* | cut -d"=" -f2 | sed 's/"//g'))
+	INLIST=($(grep THIS_SCRIPT= /etc/update-motd.d/* | cut -d"=" -f2 | sed "s/\"//g"))
 	CHOICES=$($DIALOG --separate-output --nocancel --title "Adjust welcome screen" --checklist "" 11 50 5 "${LIST[@]}" 3>&1 1>&2 2>&3)
 	INSERT="$(echo "${INLIST[@]}" "${CHOICES[@]}" | tr ' ' '\n' | sort | uniq -u | tr '\n' ' ' | sed 's/ *$//')"
 	# adjust motd config
