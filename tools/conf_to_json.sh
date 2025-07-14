eedafd3c54491b28fa0b756f583e14ecddab7d0e
tearran <tearran@gmail.com>
2025-07-14 01:21:29 +0000

    modified:   tools/conf_to_json.sh

--- tools/conf_to_json.sh
+++ tools/conf_to_json.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 conf_to_json() {
-find ./src -type f -name '*.conf' | while read -r conf; do
-	section=$(awk '/^\[.*\]/ {gsub(/^\[|\]$/, "", $0); print $0; exit}' "$conf")
-	awk -F= -v module="$section" '
-		/^[[:space:]]*#/ { next }
-		/^[[:space:]]*$/ { next }
-		/^\[/ { next }
-		/=/ {
-			gsub(/^[ \t]+|[ \t]+$/, "", $1)
-			gsub(/^[ \t]+|[ \t]+$/, "", $2)
-			keys[$1] = $2
-		}
-		END {
-			printf("{")
-			printf("\"module\":\"%s\"", module)
-			for (k in keys) printf(",\"%s\":\"%s\"", k, keys[k])
-			printf("}\n")
-		}
-	' "$conf"
-done | jq -s '
-  # for each object, use .parent, .group, .module for nesting
-  reduce .[] as $item ({};
-    .[$item.parent][$item.group][$item.module] = ($item | del(.parent, .group, .module))
-  )
-'
+	find ./src -type f -name '*.conf' | while read -r conf; do
+		section=$(awk '/^\[.*\]/ {gsub(/^\[|\]$/, "", $0); print $0; exit}' "$conf")
+		awk -F= -v module="$section" '
+			/^[[:space:]]*#/ { next }
+			/^[[:space:]]*$/ { next }
+			/^\[/ { next }
+			/=/ {
+				gsub(/^[ \t]+|[ \t]+$/, "", $1)
+				gsub(/^[ \t]+|[ \t]+$/, "", $2)
+				keys[$1] = $2
+			}
+			END {
+				printf("{")
+				printf("\"module\":\"%s\"", module)
+				for (k in keys) printf(",\"%s\":\"%s\"", k, keys[k])
+				printf("}\n")
+			}
+		' "$conf"
+	done | jq -s '
+	# for each object, use .parent, .group, .module for nesting
+	reduce .[] as $item ({};
+	.[$item.parent][$item.group][$item.module] = ($item | del(.parent, .group, .module))
+	)
+	'
 
 }
 
